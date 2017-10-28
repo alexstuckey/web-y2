c7e1fedbec0bf3a1f120a64d1bd6a7dcffd23266
Alex Stuckey <alex@stuckey.org.uk>
2017-10-28 18:08:16 +0100

    Scaffolding for two more routes

--- controllers/index.js
+++ controllers/index.js
@@ -10,6 +10,14 @@ app.get('/venues', function (req, res) {
   res.send('Hello World! venues')
 })
 
+app.post('/events', function (req, res) {
+  res.send('a POST to events')
+})
+
+app.post('/venues', function (req, res) {
+  res.send('a POST to venues')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
