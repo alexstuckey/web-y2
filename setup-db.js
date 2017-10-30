e47d9bf5d64909c4ed451fcfcabc6faf5adef1cf
Alex Stuckey <alex@stuckey.org.uk>
2017-10-30 22:49:28 +0000

    Logic to split up the multiple statements in one SQL and execute individually

--- setup-db.js
+++ setup-db.js
@@ -7,8 +7,18 @@ var db = new sqlite3.Database(destinationDBPath)
 
 fs.readFile(initialSQLPath, 'utf8', function(err, data) {
   if (err) throw err
+
+  // Break up the file into an array of statements, since SQLite cannot handle
+  // multiple statement queries.
+
+  let statements = data.split(';')
+  // There will always be an empty final element, if properly terminated SQL statement
+  statements.pop()
+
   db.serialize(function () {
-    db.run(data)
+    statements.forEach((statement) => {
+      db.run(statement)
+    })
   })
   db.close()
 })
