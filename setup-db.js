a567ae71f983d520fd5f90bbce7e1b011915a4ef
Alex Stuckey <alex@stuckey.org.uk>
2017-11-12 19:52:05 +0000

    Restructured as a module, both to be run independently and as a library

--- setup-db.js
+++ setup-db.js
@@ -2,23 +2,35 @@ const initialSQLPath = './initial-db.sql'
 const destinationDBPath = './efg.db'
 
 var fs = require('fs')
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(destinationDBPath)
 
-fs.readFile(initialSQLPath, 'utf8', function(err, data) {
-  if (err) throw err
+let main = () => {
+  fs.readFile(initialSQLPath, 'utf8', function(err, data) {
+    if (err) throw err
 
-  // Break up the file into an array of statements, since SQLite cannot handle
-  // multiple statement queries.
+    // Break up the file into an array of statements, since SQLite cannot handle
+    // multiple statement queries.
 
-  let statements = data.split(';')
-  // There will always be an empty final element, if properly terminated SQL statement
-  statements.pop()
+    let statements = data.split(';')
+    // There will always be an empty final element, if properly terminated SQL statement
+    statements.pop()
 
-  db.serialize(function () {
-    statements.forEach((statement) => {
-      db.run(statement)
+    var sqlite3 = require('sqlite3').verbose()
+    var db = new sqlite3.Database(destinationDBPath)
+
+    db.serialize(function () {
+      statements.forEach((statement) => {
+        db.run(statement)
+      })
     })
+    db.close()
   })
-  db.close()
-})
+}
+
+
+if (!module.parent) {
+  // ran as `node setup-db.js`
+  main()
+} else {
+  // required from another script
+  module.exports = { createDatabase: main }
+}
