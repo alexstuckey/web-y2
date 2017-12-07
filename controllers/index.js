90efe67025e2f11d5a7132cbf394aeb9f3d4b745
Alex Stuckey <alex@stuckey.org.uk>
2017-12-07 17:22:37 +0000

    Importing request and qs

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const app = express()
 const config = require('../config.js')
 const crypto = require('crypto')
 const fs = require('fs')
+const request = require('request')
+const qs = require('qs')
 const parseXMLString = require('xml2js').parseString
 const setupDB = require('../setup-db.js')
 
